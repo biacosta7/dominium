6c45560a81424e336e637d3e4d71aa532500eba6
ninahffbs <nina.hffs@gmail.com>
2026-04-21 00:33:20 -0300

    feat: deletar unidade

--- backend/src/main/java/com/dominium/backend/delivery/rest/unidade/UnidadeController.java
+++ backend/src/main/java/com/dominium/backend/delivery/rest/unidade/UnidadeController.java
@@ -3,15 +3,13 @@ package com.dominium.backend.delivery.rest.unidade;
 import com.dominium.backend.application.unidade.dto.UnidadeRequestDTO;
 import com.dominium.backend.application.unidade.dto.UnidadeResponseDTO;
 import com.dominium.backend.application.unidade.usecase.CreateUnidadeUseCase;
-// import com.dominium.backend.application.unidade.usecase.DeleteUnidadeUseCase;
+import com.dominium.backend.application.unidade.usecase.DeleteUnidadeUseCase;
 // import com.dominium.backend.application.unidade.usecase.GetUnidadeUseCase;
 // import com.dominium.backend.application.unidade.usecase.UpdateUnidadeUseCase;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @RestController
 @RequestMapping("/unidades")
@@ -19,16 +17,22 @@ public class UnidadeController {
     private final CreateUnidadeUseCase createUnidadeUseCase;
 //     private final GetUnidadeUseCase getUnidadeUseCase;
 //     private final UpdateUnidadeUseCase updateUnidadeUseCase;
-//     private final DeleteUnidadeUseCase deleteUnidadeUseCase;
+    private final DeleteUnidadeUseCase deleteUnidadeUseCase;
 
-    public UnidadeController(CreateUnidadeUseCase createUnidadeUseCase){
+    public UnidadeController(CreateUnidadeUseCase createUnidadeUseCase, DeleteUnidadeUseCase deleteUnidadeUseCase){
         this.createUnidadeUseCase = createUnidadeUseCase;
+        this.deleteUnidadeUseCase = deleteUnidadeUseCase;
     }
     @PostMapping
     public ResponseEntity<UnidadeResponseDTO> create(@Valid @RequestBody UnidadeRequestDTO request){
         UnidadeResponseDTO response = createUnidadeUseCase.execute(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        deleteUnidadeUseCase.execute(id);
+        return ResponseEntity.noContent().build();
+    }   
 }
 
 //     @GetMapping
