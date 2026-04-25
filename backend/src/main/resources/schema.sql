76465b3840a100e5898d6555f301dafa876898fa
Sofia1653 <sofedu2005@gmail.com>
2026-04-25 09:06:27 -0300

    feat: adicionando tabelas

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -34,3 +34,36 @@ CREATE TABLE IF NOT EXISTS vinculos_morador (
     CONSTRAINT fk_vinculo_unidade FOREIGN KEY (unidade_id) REFERENCES unidades(id),
     CONSTRAINT fk_vinculo_usuario FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
+
+CREATE TABLE IF NOT EXISTS pauta (
+    id BIGSERIAL PRIMARY KEY,
+    assembleia_id BIGINT NOT NULL,
+    titulo VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    tipo_quorum VARCHAR(50)  NOT NULL,
+    tipo_maioria VARCHAR(50)  NOT NULL,
+    status VARCHAR(50)  NOT NULL DEFAULT 'ABERTA',
+    resultado VARCHAR(50)  NOT NULL DEFAULT 'EM_ANDAMENTO'
+    );
+
+CREATE TABLE IF NOT EXISTS voto (
+    id BIGSERIAL PRIMARY KEY,
+    pauta_id BIGINT NOT NULL REFERENCES pauta(id),
+    unidade_id BIGINT NOT NULL,
+    usuario_id BIGINT NOT NULL,
+    opcao_voto VARCHAR(50) NOT NULL,
+
+    -- Uma unidade só pode votar uma vez por pauta
+    CONSTRAINT uk_voto_pauta_unidade UNIQUE (pauta_id, unidade_id)
+    );
+
+CREATE TABLE IF NOT EXISTS reserva (
+    id  VARCHAR(36) PRIMARY KEY,  -- UUID gerado pelo domínio
+    area_comum_id BIGINT NOT NULL,
+    unidade_id BIGINT NOT NULL REFERENCES unidade(id),
+    usuario_id BIGINT NOT NULL REFERENCES usuario(id),
+    data_reserva DATE NOT NULL,
+    hora_inicio TIME NOT NULL,
+    hora_fim TIME NOT NULL,
+    status VARCHAR(50) NOT NULL DEFAULT 'PENDENTE'
+    );
