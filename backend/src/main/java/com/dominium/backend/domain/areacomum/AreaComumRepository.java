fee28e7e352e8bd8b3daf5472c33ad19abe01b88
Beatriz Costa <beatriz.costa@liferay.com>
2026-04-26 19:48:34 -0300

    Ajuste no Repository de Area comum

--- backend/src/main/java/com/dominium/backend/domain/areacomum/AreaComumRepository.java
+++ backend/src/main/java/com/dominium/backend/domain/areacomum/AreaComumRepository.java
@@ -1,10 +1,10 @@
 package com.dominium.backend.domain.areacomum;
 
-import com.dominium.backend.domain.areacomum.AreaComum;
-
 import java.util.Optional;
 
 public interface AreaComumRepository {
 
     Optional<AreaComum> findById(AreaComumId id);
+
+    AreaComum save(AreaComum areaComum);
 }
