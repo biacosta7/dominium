436e5f5e06aae9f30d1717331df732f833be33eb
gabsvelozo <gabsmastellari@gmail.com>
2026-04-26 23:22:47 -0300

    fix: add component scan, disable security autoconfigure and ignore data.sql on startup

--- gestaoCondominial/apresentacao-backend/src/main/java/br/com/cesar/gestaoCondominial/apresentacao/dominium/BackendApplication.java
+++ gestaoCondominial/apresentacao-backend/src/main/java/br/com/cesar/gestaoCondominial/apresentacao/dominium/BackendApplication.java
@@ -3,7 +3,7 @@ package br.com.cesar.gestaoCondominial.apresentacao.dominium;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.com.cesar.gestaoCondominial")
 public class BackendApplication {
 
 	public static void main(String[] args) {
